f3b0e9c87807e22dcfc232864b0359914ed9811c
Şerife <eyaman895@gmail.com>
2021-01-20 00:34:24 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Eklenen müşteri: " + musteri.Ad + " " + musteri.Soyad);
+        }
+        public void Listele(Musteri[] musteriDizisi)
+        {
+            Console.WriteLine("****Müşteri Listesi****");
+            foreach (Musteri musteri in musteriDizisi)
+            {
+                Console.WriteLine("AD: " + musteri.Ad);
+                Console.WriteLine("SOYAD: " + musteri.Soyad);
+                Console.WriteLine("TC NO: " + musteri.MusteriTC);
+                Console.WriteLine("**********************");
+            }
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Silinen müşteri: " + musteri.Ad + " " + musteri.Soyad);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Şerife";
+            musteri1.Soyad = "YAMAN";
+            musteri1.MusteriTC = "12345678910";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Eren";
+            musteri2.Soyad = "YAMAN";
+            musteri2.MusteriTC = "55558896310";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Ali";
+            musteri3.Soyad = "VELİ";
+            musteri3.MusteriTC = "22553678954";
+
+            MusteriManager islemler = new MusteriManager();
+
+            islemler.Ekle(musteri1);                        //parametre olarak verilen müşteriyi ekler
+            islemler.Sil(musteri2);                         //parametre olarak verilen müşteriyi siler
+
+            Musteri[] musteriler = new Musteri[] {musteri1, musteri2, musteri3};
+            islemler.Listele(musteriler);                   //tüm müşterileri listeler
+
+
+        }
+
+       
+    }
+}
